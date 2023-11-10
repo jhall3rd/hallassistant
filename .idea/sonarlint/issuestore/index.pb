f3030c94ca472c8a1726b74cf91104b6d5d2b3aa
Chris Brew <brewc@dy6nm2ng4q.legal.regn.net>
2023-11-10 06:32:51 -0500

    Demo 5 is ready

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -21,8 +21,6 @@ Q.idea/sonarlint/securityhotspotstore/a/5/a5cc2925ca8258af241be7e5b0381edf302663
 ^
 ..idea/inspectionProfiles/profiles_settings.xml,1/e/1e9075f5bf079c01ef2c910709e91a497d262080
 Å
-Q.idea/sonarlint/securityhotspotstore/f/1/f1bdda93d9a278e358509d498e17d97764c1fb29,b/5/b58a0677868503201074e965423d875803239017
-Å
 Q.idea/sonarlint/securityhotspotstore/3/c/3c84dcdc6bbe3d7817c49dcdc327b926fea1808a,4/e/4ead0a9bb92d8636c4ad5a0ef56838d8b7bd33d6
 \
 ,.idea/inspectionProfiles/Project_Default.xml,4/9/496a238a6afa168dbaf6efd37bb459331589579c
--- demo5.py
+++ demo5.py
@@ -2,122 +2,128 @@
 This demo shows the use of asyncio to structure client and server interaction
 with OpenAI clients.
 
-TODO: better organization of IO
-
 """
 
 import asyncio
+import json
 import os
+import random
+from typing import Tuple
+
 import dotenv
 from openai import OpenAI
 from util import poll_run, print_messages
 
 
+def get_weather(location):
+    match location:
+        case "London":
+            return "12C"
+        case "Paris":
+            return "15C"
+        case "Jakarta":
+            return "20C"
+        case _:
+            return f"{random.randint(0,20)}C"
 
-def prepare_openai():
-    dotenv.load_dotenv()
-    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
-    assistant = client.beta.assistants.create(
-        instructions="You are a weather bot. Use the provided functions to answer questions.",
-        model="gpt-4-1106-preview",
-        tools=[{
-            "type": "function",
-            "function": {
-                "name": "getCurrentWeather",
-                "description": "Get the weather in location",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "location": {"type": "string", "description": "The city and state e.g. San Francisco, CA"},
-                        "unit": {"type": "string", "enum": ["c", "f"]}
+class Session:
+    def __init__(self):
+        dotenv.load_dotenv()
+        client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+        assistant = client.beta.assistants.create(
+            instructions="You are a weather bot. Use the provided functions to answer questions.",
+            model="gpt-4-1106-preview",
+            tools=[
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "getCurrentWeather",
+                        "description": "Get the weather in location",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "location": {
+                                    "type": "string",
+                                    "description": "The city and state e.g. San Francisco, CA",
+                                },
+                                "unit": {"type": "string", "enum": ["c", "f"]},
+                            },
+                            "required": ["location"],
+                        },
                     },
-                    "required": ["location"]
-                }
-            }
-        }, {
-            "type": "function",
-            "function": {
-                "name": "getNickname",
-                "description": "Get the nickname of a city",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "location": {"type": "string", "description": "The city and state e.g. San Francisco, CA"},
+                },
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "getNickname",
+                        "description": "Get the nickname of a city",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "location": {
+                                    "type": "string",
+                                    "description": "The city and state e.g. San Francisco, CA",
+                                },
+                            },
+                            "required": ["location"],
+                        },
                     },
-                    "required": ["location"]
-                }
-            }
-        }]
-    )
+                },
+            ],
+        )
 
-    thread = client.beta.threads.create()
-
-
-    return client,assistant,thread
-
-async def run_openai(msg, client,assistant,thread,q_in, q_out):
-    """
-    Doesn't quite work.
-
-    :param msg:
-    :param client:
-    :param assistant:
-    :param thread:
-    :return:
-    """
-
-    client.beta.threads.messages.create(
-        thread_id=thread.id,
-        role="user",
-        content=msg,
-    )
-
-    run = client.beta.threads.runs.create(
-        thread_id=thread.id,
-        assistant_id=assistant.id,
-        instructions="Please address the user as Alfred Clement.",
-    )
-
-    run = await poll_run(client, thread_id=thread.id, run_id=run.id)
-
-    if run.status == "completed":
-        messages = client.beta.threads.messages.list(thread_id=thread.id)
-        print_messages(messages)
-    elif run.status == "requires_action":
-        # we don't plan to really run a function. Instead we will ask the
-        # user for a value.
+        thread = client.beta.threads.create()
+        self.client,self.assistant,self.thread = client,assistant,thread
+        self.run = None
 
 
+    @property
+    def status(self):
+        return self.run and self.run.status
 
+    async def start_run(self,message):
+        self.client.beta.threads.messages.create(
+            thread_id=self.thread.id,
+            role="user",
+            content=message,
+        )
 
+        self.run = self.client.beta.threads.runs.create(
+            thread_id=self.thread.id,
+            assistant_id=self.assistant.id,
+            instructions="Please address the user as Alfred Clement.",
+        )
+        self.run = await poll_run(self.client,
+                                  thread_id=self.thread.id,
+                                  run_id=self.run.id)
 
-        tool_call = run.required_action.submit_tool_outputs.tool_calls[0]
+    def get_result(self):
+        assert self.run.status == "completed"
+        messages = self.client.beta.threads.messages.list(thread_id=self.thread.id)
+        message = messages.data[0].content[0].text.value
+        return message
 
-        run = client.beta.threads.runs.submit_tool_outputs(
-            thread_id=thread.id,
-            run_id=run.id,
+    async def do_required_action(self):
+        assert self.status == "requires_action"
+        tool_call = self.run.required_action.submit_tool_outputs.tool_calls[0]
+        run = self.client.beta.threads.runs.submit_tool_outputs(
+            thread_id= self.thread.id,
+            run_id=self.run.id,
             tool_outputs=[
                 {
                     "tool_call_id": tool_call.id,
-                    "output": "22C",
+                    "output": get_weather(json.loads(tool_call.function.arguments)['location']),
                 }
-            ]
+            ],
         )
-        run = await poll_run(client, thread_id=thread.id, run_id=run.id)
-        assert run.status == "completed"  # maybe not, but error handling is dull.
-        messages = client.beta.threads.messages.list(thread_id=thread.id)
-        message =  messages.data[0].content[0].text.value
-        return message
-
-    else:
-        print(run.status)
+        self.run = await poll_run(self.client, thread_id=self.thread.id, run_id=run.id)
+        return self.get_result()
 
 
 
-
-
-async def server(q_in: asyncio.Queue, q_out: asyncio.Queue):
+async def run_server(q_in: asyncio.Queue, q_out: asyncio.Queue):
     """
     Stub for openai LLM.
 
@@ -125,31 +131,30 @@ async def server(q_in: asyncio.Queue, q_out: asyncio.Queue):
     :param q_out:
     :return:
     """
-
-    client,assistant,thread = prepare_openai()
+    session = Session()
     while True:
         msg = await q_in.get()
-        msg_in = msg[msg.index(" ")+1:]
-        msg_out = await run_openai(msg_in,client,assistant,thread, q_in, q_out)
+        msg_in = msg[msg.index(" ") + 1 :]
+        await session.start_run(msg_in)
+        if session.status == "completed":
+            msg_out = session.get_result()
+        elif session.status == "requires_action":
+            msg_out = await session.do_required_action()
+        else:
+            msg_out = session.status
         await q_out.put(msg_out)
 
 
-async def client(name: int,
-                 q_in: asyncio.Queue,
-                 q_out: asyncio.Queue):
+async def run_client(name: int, q_in: asyncio.Queue, q_out: asyncio.Queue):
     while True:
         try:
             text = input("[H] ").strip()
-            if text in ('!q',"bye"):
+            if text in ("!q", "bye"):
                 break
             elif text:
                 await q_out.put(f"[A] {text}")
                 msg = await q_in.get()
-                if msg.startswith("[T]"):
-                    tp = input("[T] ")
-                    await q_out.put(tp)
-                else:
-                    print(msg)
+                print('[A]',msg)
         except EOFError:
             break
 
@@ -160,10 +165,11 @@ async def main():
     q1 = asyncio.Queue()  # messages pass from client to server
     q2 = asyncio.Queue()  # messages pass from server to client
 
-    client1 = asyncio.create_task(client(name=1, q_in=q2, q_out=q1))
-    server1 = asyncio.create_task(server(q_in=q1, q_out=q2))
+    client1 = asyncio.create_task(run_client(name=1, q_in=q2, q_out=q1))
+    server1 = asyncio.create_task(run_server(q_in=q1, q_out=q2))
     await asyncio.gather(client1)
     server1.cancel()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
