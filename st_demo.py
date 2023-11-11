d61d8e3b63f18cb5b930fb78a31970660e50dc9e
Chris Brew <brewc@dy6nm2ng4q.legal.regn.net>
2023-11-11 09:49:21 -0500

    Main change is demo6

--- st_demo.py
+++ st_demo.py
@@ -1,67 +1,26 @@
 import asyncio
-import pandas as pd
-import plotly.express as px
+
 
 import streamlit as st
-from datetime import datetime
 
-CHOICES = [1, 2, 3]
+
+
 
 
 def main():
-    print('\nmain...')
+    st.set_page_config(layout="wide")
+    col1,col2 = st.columns(2)
+    with col1:
+        st.header("Client")
+        container1 = st.empty()
+        container2 = st.empty()
+    with col2:
+        st.header("Server")
+    asyncio.run(run_server(container1,container2))
 
-    # layout your app beforehand, with st.empty
-    # for the widgets that the async function would populate
-    graph = st.empty()
-    radio = st.radio('Choose', CHOICES, horizontal=True)
-    table = st.empty()
+async def run_server(container1,container2):
+    pass
 
-    try:
-        # async run the draw function, sending in all the
-        # widgets it needs to use/populate
-        asyncio.run(draw_async(radio, graph, table))
-    except Exception as e:
-        print(f'error...{type(e)}')
-        raise
-    finally:
-        # some additional code to handle user clicking stop
-        print('finally')
-        # this doesn't actually get called, I think :(
-        table.write('User clicked stop!')
-
-
-async def draw_async(choice, graph, table):
-    # must send in all the streamlit widgets that
-    # this fn would interact with...
-
-    # this could possibly work, but layout is tricky
-    # choice2 = st.radio('Choose 2', CHOICES)
-
-    while True:
-        # this would not work because you'd be creating duplicated
-        # radio widgets
-        # choice3 = st.radio('Choose 3', CHOICES)
-
-        timestamp = datetime.now()
-        sec = timestamp.second
-
-        graph_df = pd.DataFrame({
-            'x': [0, 1, 2],
-            'y': [max(CHOICES), choice, choice * sec / 60.0],
-            'color': ['max', 'current', 'ticking']
-        })
-
-        df = pd.DataFrame({
-            'choice': CHOICES,
-            'current_choice': len(CHOICES) * [choice],
-            'time': len(CHOICES) * [timestamp]
-        })
-
-        graph.plotly_chart(px.bar(graph_df, x='x', y='y', color='color'))
-        table.dataframe(df)
-
-        _ = await asyncio.sleep(1)
 
 
 if __name__ == '__main__':
